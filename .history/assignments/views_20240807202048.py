7d6fe89d4300c9f2b042cfae4c305afad0bdf991
vijay gholve <vijaygholve77c@gmail.com>
2024-08-07 22:27:55 +0530

    UP

---
+++ .history/assignments/views_20240807202048.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render ,redirect ,get_object_or_404 
+from .forms import AssignmentForm 
+from django.contrib.messages import error
+from students.models import Teacher ,CustomUser ,Assignment  ,ClassGroup ,Classes 
+from django.http import FileResponse, Http404 ,HttpResponse 
+from .task import send_mail_to_all_students_for_assignemt 
+import os 
+
+
+def all_classes(request):
+    classes=Classes.objects.all()
+    class1=Classes.objects.get(name="BA")
+    year=ClassGroup.objects.filter(name=class1)
+    return render(request,"assignements\create_assignment.html")
+def all_year_list(request ,class_id):
+    classes=ClassGroup.objects.filter(name__id=class_id)
+    content={"classes":classes} 
+    return render(request,"assignements/assignemts_year_list.html",content)
+def download_file(request, file_id):
+    downloadable_file = get_object_or_404(Assignment, id=file_id)
+    file_path = downloadable_file.description.path 
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file: 
+            response = HttpResponse(file.read(), content_type='application/octet-stream')
+            response['Content-Disposition'] = f'attachment; filename={os.path.basename(file_path)}'
+            return response
+    else:
+        raise Http404("File not found")
+def assignemt_form(request):
+    form=AssignmentForm() 
+    # teacher=Teacher.objects.get(name="hod2")
+    if request.method == "POST":
+        form=AssignmentForm(request.POST,request.FILES)
+        if form.is_valid():
+            assign=form.save(commit=False)
+            
+            assign.save()
+            send_mail_to_all_students_for_assignemt.delay(assign.id)  
+            return redirect('home') 
+        else:
+            error(request, "Invalid assignment")
+    return render(request,rf'assignements\create_assignment.html', {'form': form})
+
+def assignement_home(request,class_id):
+    classgroup=ClassGroup.objects.get(id=class_id)
+    assignments=Assignment.objects.filter(classgroup=classgroup) 
+    content={'assignments': assignments} 
+    return render(request,rf'assignements\assignemts_home.html',content) 
+
+
+    
---
+++ .history/assignments/views_20240807204529.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render ,redirect ,get_object_or_404 
+from .forms import AssignmentForm 
+from django.contrib.messages import error
+from students.models import Teacher ,CustomUser ,Assignment  ,ClassGroup ,Classes 
+from django.http import FileResponse, Http404 ,HttpResponse 
+from .task import send_mail_to_all_students_for_assignemt 
+import os 
+
+
+def all_classes(request):
+    classes = Classes.objects.all()
+    years = ClassGroup.objects.all()
+
+    class_id = request.GET.get('class')
+    year_id = request.GET.get('year')
+
+    if class_id and year_id:
+        selected_class = get_object_or_404(Classes, id=class_id)
+        selected_year = get_object_or_404(ClassGroup, id=year_id)
+        # Assuming you have a view named 'home-assignments' that expects class and year IDs.
+        return redirect('home-assignments', class_id=selected_class.id, year_id=selected_year.id)
+    
+    content = {"classes": classes, "years": years}
+    return render(request,""assignements\assignemts_classes_list.html", content)
+
+def all_year_list(request ,class_id):
+    classes=ClassGroup.objects.filter(name__id=class_id)
+    content={"classes":classes} 
+    return render(request,"assignements/assignemts_year_list.html",content)
+def download_file(request, file_id):
+    downloadable_file = get_object_or_404(Assignment, id=file_id)
+    file_path = downloadable_file.description.path 
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file: 
+            response = HttpResponse(file.read(), content_type='application/octet-stream')
+            response['Content-Disposition'] = f'attachment; filename={os.path.basename(file_path)}'
+            return response
+    else:
+        raise Http404("File not found")
+def assignemt_form(request):
+    form=AssignmentForm() 
+    # teacher=Teacher.objects.get(name="hod2")
+    if request.method == "POST":
+        form=AssignmentForm(request.POST,request.FILES)
+        if form.is_valid():
+            assign=form.save(commit=False)
+            
+            assign.save()
+            send_mail_to_all_students_for_assignemt.delay(assign.id)  
+            return redirect('home') 
+        else:
+            error(request, "Invalid assignment")
+    return render(request,rf'assignements\create_assignment.html', {'form': form})
+
+def assignement_home(request,class_id):
+    classgroup=ClassGroup.objects.get(id=class_id)
+    assignments=Assignment.objects.filter(classgroup=classgroup) 
+    content={'assignments': assignments} 
+    return render(request,rf'assignements\assignemts_home.html',content) 
+
+
+    
