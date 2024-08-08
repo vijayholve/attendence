365be195341e2795b7cc2bc4a1f45d7eb36ce8ca
vijay gholve <vijaygholve77c@gmail.com>
2024-08-09 00:01:34 +0530

    UP

---
+++ .history/assignments/urls_20240808235046.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from . import views
+urlpatterns = [
+    path("all-classes-assignements/",views.all_classes,name="all-classes-assignements"),
+    path("all-year-assignements/<int:class_id>/",views.all_year_list,name="all-year-assignements"),
+    path('create-assignment/',views.assignemt_form,name="create-assignment"),
+    path('home-assignemts/<str:class_id>/',views.assignement_home,name="home-assignemts"),
+    path('download/<int:file_id>/', views.download_file, name='download_file'),
+    path('assignemnt-check/<int:class_id>/', views.assignments_checking, name='download_file'),
+
+    
+ 
+]
